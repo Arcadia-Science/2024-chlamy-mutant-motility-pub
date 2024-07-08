29ee4d2f4a1dea85cbd9c6f107617f5f5ff8f41e
lanery <ryanilane@gmail.com>
2024-07-08 13:12:07 -0700

    Update compute_motility_metrics.py

--- src/scripts/compute_motility_metrics.py
+++ src/scripts/compute_motility_metrics.py
@@ -1,5 +1,4 @@
 import json
-import re
 from pathlib import Path
 
 import click
@@ -12,22 +11,86 @@ INPUT_DIRECTORY = Path(__file__).parents[2] / "data/cell_trajectory_csvs/"
 INPUT_JSON = Path(__file__).parents[2] / "data/experimental_parameters.json"
 OUTPUT_CSV = Path(__file__).parents[2] / "data/summary_motility_metrics.csv"
 
+trajectory_time_threshold_option = click.option(
+    "--time-threshold",
+    "time_threshold",
+    default=10,
+    show_default=True,
+    help=(
+        "Minimum trajectory duration (in seconds). Motility metrics from cells with a shorter "
+        "trajectory duration than this threshold will be discarded."
+    ),
+)
 
+trajectory_distance_threshold_option = click.option(
+    "--distance-threshold",
+    "distance_threshold",
+    default=20,
+    show_default=True,
+    help=(
+        "Minimum trajectory distance (in microns). Motility metrics from cells that traverse a "
+        "shorter distance than this threshold will be discarded."
+    ),
+)
+
+
+@trajectory_distance_threshold_option
+@trajectory_time_threshold_option
 @click.command()
-def main():
+def main(time_threshold, distance_threshold):
     """"""
+    # handle missing file paths
+    if not INPUT_DIRECTORY.exists():
+        msg = "Input directory for CSV files of cell trajectories not found."
+        raise FileNotFoundError(msg)
 
-    print("input_directory ::", INPUT_DIRECTORY)
-    print("input_json ::", INPUT_JSON)
-    print("output_csv ::", OUTPUT_CSV)
+    if not INPUT_JSON.exists():
+        msg = "Input json file for experimental parameters not found."
+        raise FileNotFoundError(msg)
 
-    # experimental_parameters = json.loads
+    # load experimental parameters
+    experimental_parameters = json.loads(INPUT_JSON.read_text())
+    framerate = experimental_parameters["framerate"]
+    pixelsize = experimental_parameters["pixelsize"]
 
-    # glob csvs
+    # collect CSV files to process
+    trajectory_csvs = natsorted(INPUT_DIRECTORY.glob("*.csv"))
+    # initialize dataframe of summary motility metrics
+    motility_metrics_dataframe = pd.DataFrame()
 
-    # filter
+    for csv in tqdm(trajectory_csvs):
+        # extract well ID from CSV filename
+        well_id = csv.name.split("_")[0]
+
+        # parse motility data from CSV
+        cell_trajectories = TrajectoryCSVParser(csv, framerate, pixelsize)
+
+        # estimate cell count and compute motility measurements for a batch of cell trajectories
+        cell_count = cell_trajectories.estimate_cell_count()
+        motility_metrics = cell_trajectories.compute_summary_statistics()
+        dataframe = pd.DataFrame(motility_metrics)
+
+        # build up dataframe
+        dataframe["cell_count"] = cell_count
+        dataframe["strain"] = experimental_parameters[well_id]["strain"]
+        dataframe["drug"] = experimental_parameters[well_id]["drug"]
+        dataframe["concentration"] = experimental_parameters[well_id]["concentration"]
+        dataframe["well_ID"] = well_id
+
+        # concatenate batch of motility metrics
+        motility_metrics_dataframe = pd.concat([motility_metrics_dataframe, dataframe])
+
+    # clean up dataframe and apply thresholds
+    motility_metrics_dataframe = motility_metrics_dataframe.drop("cell_id", axis=1).reset_index(
+        drop=True
+    )
+    motility_metrics_dataframe_filtered = motility_metrics_dataframe.loc[
+        (motility_metrics_dataframe["total_time"] >= time_threshold)
+        & (motility_metrics_dataframe["total_distance"] >= distance_threshold)
+    ]
 
     # export
+    motility_metrics_dataframe_filtered.to_csv(OUTPUT_CSV, index=False)
 
 
 if __name__ == "__main__":
