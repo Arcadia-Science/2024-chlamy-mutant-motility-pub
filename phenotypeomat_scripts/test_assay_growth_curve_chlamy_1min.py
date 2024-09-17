63d4acbb2f2491f25c5efd084e348ebe642e9278
lanery <ryanilane@gmail.com>
2024-09-17 10:58:25 -0700

    move phenotype-o-mat scripts
    Make it obvious that these scripts are for setting up acquisitions with the phenotype-o-mat

--- phenotypeomat_scripts/test_assay_growth_curve_chlamy_1min.py
+++ phenotypeomat_scripts/test_assay_growth_curve_chlamy_1min.py
@@ -1,20 +1,22 @@
-# Import necessary modules
 import sys as sys
-sys.path.insert(0, '/home/arcadia/git/arcadia-phenotypomat/phenotyper/')  # Add the path for phenotyper tools to the system path
-import cam_tools as ct  # Import camera tools for interacting with the Blackfly camera
-import matplotlib.pyplot as plt  # Import for potential plotting, though not used in this code
-import serial as ser  # Import for serial communication (to control the LED and grow lights)
+
+sys.path.insert(
+    0, "/home/arcadia/git/arcadia-phenotypomat/phenotyper/"
+)  # Add the path for phenotyper tools to the system path
 import time as tm  # Import to handle delays (e.g., between frames)
+
+import cam_tools as ct  # Import camera tools for interacting with the Blackfly camera
+import serial as ser  # Import for serial communication (to control the LED and grow lights)
 from pyzbar.pyzbar import decode  # Import to decode barcodes from the images
 
 # Establish serial communication with the microcontroller (controls the LEDs and grow lights)
-dev = ser.Serial('/dev/ttyACM0')  
+dev = ser.Serial("/dev/ttyACM0")
 
 # Turn off the transmitted light (LED) before starting the imaging process
-dev.write(b'SET LED_TRANS_STATUS 1;')
+dev.write(b"SET LED_TRANS_STATUS 1;")
 
 # Ensure that Blackfly cameras are connected to the system
-if not ct.detect_cams():  
+if not ct.detect_cams():
     sys.exit()
 
 # Start communication with the Blackfly camera system
@@ -33,9 +35,9 @@ cam.Init()
 ct.set_expos_time(cam, 500)
 
 # Turn on the transmitted light (LED) and grow lights (460 nm and 670 nm LEDs)
-dev.write(b'SET LED_TRANS_STATUS 0;')  # Turn on the transmitted light
-dev.write(b'SET LED_460_STATUS 0;')  # Turn on 460 nm grow light
-dev.write(b'SET LED_670_STATUS 0;')  # Turn on 670 nm grow light
+dev.write(b"SET LED_TRANS_STATUS 0;")  # Turn on the transmitted light
+dev.write(b"SET LED_460_STATUS 0;")  # Turn on 460 nm grow light
+dev.write(b"SET LED_670_STATUS 0;")  # Turn on 670 nm grow light
 
 # Capture the first image and get the timestamp
 image, timestamp = ct.grab_images(cam, n_frames=1)
@@ -43,40 +45,40 @@ image, timestamp = ct.grab_images(cam, n_frames=1)
 # Attempt to decode a barcode from the first image to identify the sample
 bcode = decode(image[0])
 if not bcode:  # If no barcode is found, use a default value '00000'
-    bcode = '00000'
+    bcode = "00000"
 else:  # Otherwise, use the decoded barcode data
     bcode = bcode.data
 
 # Loop through 96 cycles (for each well in the 96-well plate)
-for n in range(0, 96):
+for _ in range(0, 96):
     # Reset the exposure time for the camera to 500 ms
     ct.set_expos_time(cam, 500)
-    
+
     # Turn off grow lights (460 nm and 670 nm) before imaging
-    dev.write(b'SET LED_460_STATUS 1;')  # Turn off 460 nm grow light
-    dev.write(b'SET LED_670_STATUS 1;')  # Turn off 670 nm grow light
-    
+    dev.write(b"SET LED_460_STATUS 1;")  # Turn off 460 nm grow light
+    dev.write(b"SET LED_670_STATUS 1;")  # Turn off 670 nm grow light
+
     # Turn on the transmitted light (LED) for imaging
-    dev.write(b'SET LED_TRANS_STATUS 0;')  
-    
+    dev.write(b"SET LED_TRANS_STATUS 0;")
+
     # Wait for 5 seconds to stabilize lighting
     tm.sleep(5)
-    
+
     # Capture an image with the camera and get the timestamps
     images, timestamps = ct.grab_images(cam, n_frames=1)
-    
+
     # Save the captured images as an AVI video, using the barcode for file identification
-    ct.save_avi(images, barcode=bcode, prefix='algae_growth_curve')
-    
+    ct.save_avi(images, barcode=bcode, prefix="algae_growth_curve")
+
     # Turn the grow lights (460 nm and 670 nm LEDs) back on after imaging
-    dev.write(b'SET LED_460_STATUS 0;')  
-    dev.write(b'SET LED_670_STATUS 0;')
-    
+    dev.write(b"SET LED_460_STATUS 0;")
+    dev.write(b"SET LED_670_STATUS 0;")
+
     # Wait for 1 minute before capturing the next image
     tm.sleep(60 * 1)
-    
+
     # Turn off the transmitted light (LED) after each imaging cycle
-    dev.write(b'SET LED_TRANS_STATUS 1;)
+    dev.write(b"SET LED_TRANS_STATUS 1;")
 
 # Print the total imaging time (difference between the first and last timestamps)
 print(str(timestamps[-1] - timestamps[0]))
@@ -85,7 +87,7 @@ print(str(timestamps[-1] - timestamps[0]))
 print(str(timestamps[-1] - timestamps[-2]))
 
 # Turn off the transmitted light (LED) at the end of the experiment
-dev.write(b'SET LED_TRANS_STATUS 1;)
+dev.write(b"SET LED_TRANS_STATUS 1;")
 
 # Clean up: release the camera and the system handlers
 del cam  # De-initialize the camera
--- phenotypeomat_scripts/test_assay_growth_curve_chlamy_4d.py
+++ phenotypeomat_scripts/test_assay_growth_curve_chlamy_4d.py
@@ -1,30 +1,32 @@
-# Import necessary modules
 import sys as sys
-sys.path.insert(0,'/home/arcadia/git/arcadia-phenotypomat/phenotyper/')  # Add the path for phenotyper tools to the system path
-import cam_tools as ct  # Import camera tools for interacting with the Blackfly camera
-import matplotlib.pyplot as plt  # Import for potential plotting, though not used in this code
-import serial as ser  # Import for serial communication (to control the LED and grow lights)
+
+sys.path.insert(
+    0, "/home/arcadia/git/arcadia-phenotypomat/phenotyper/"
+)  # Add the path for phenotyper tools to the system path
 import time as tm  # Import to handle delays (e.g., between frames)
+
+import cam_tools as ct  # Import camera tools for interacting with the Blackfly camera
+import serial as ser  # Import for serial communication (to control the LED and grow lights)
 from pyzbar.pyzbar import decode  # Import to decode barcodes from the images
 
 # Establish serial communication with the microcontroller to control the LEDs
-dev = ser.Serial('/dev/ttyACM0')  # Set up a serial connection to the microcontroller
+dev = ser.Serial("/dev/ttyACM0")  # Set up a serial connection to the microcontroller
 
 # Turn off the LED before beginning imaging
-dev.write(b'SET LED_TRANS_STATUS 1;')
+dev.write(b"SET LED_TRANS_STATUS 1;")
 
 # Check if any cameras are connected; if not, exit the program
-if not ct.detect_cams():  
+if not ct.detect_cams():
     sys.exit()
 
 # Start communication with the camera system (Blackfly cameras)
-system = ct.ps.System.GetInstance()  
+system = ct.ps.System.GetInstance()
 
 # Get a list of all connected cameras
 cam_list = system.GetCameras()
 
 # Select the first camera (index 0)
-cam = cam_list[0]  
+cam = cam_list[0]
 
 # Initialize the selected camera
 cam.Init()
@@ -33,11 +35,11 @@ cam.Init()
 ct.set_expos_time(cam, 500)
 
 # Turn on the white LED for transmitted light imaging
-dev.write(b'SET LED_TRANS_STATUS 0;')
+dev.write(b"SET LED_TRANS_STATUS 0;")
 
 # Turn on grow lights (460 nm and 670 nm LEDs) for algae growth
-dev.write(b'SET LED_460_STATUS 0;')
-dev.write(b'SET LED_670_STATUS 0;')
+dev.write(b"SET LED_460_STATUS 0;")
+dev.write(b"SET LED_670_STATUS 0;")
 
 # Capture an image from the camera and extract the timestamp
 image, timestamp = ct.grab_images(cam, n_frames=1)
@@ -45,49 +47,49 @@ image, timestamp = ct.grab_images(cam, n_frames=1)
 # Attempt to read a barcode from the first image to identify the sample
 bcode = decode(image[0])
 if not bcode:  # If no barcode is detected, use a default value '00000'
-    bcode = '00000'
+    bcode = "00000"
 else:  # Otherwise, use the decoded barcode data
     bcode = bcode.data
 
 # Loop through 96 cycles (for each well in the plate)
-for n in range(0, 96):
+for _ in range(0, 96):
     # Reset the exposure time for the camera
     ct.set_expos_time(cam, 500)
-    
+
     # Turn off grow lights (460 nm and 670 nm LEDs) before imaging
-    dev.write(b'SET LED_460_STATUS 1;')  
-    dev.write(b'SET LED_670_STATUS 1;')  
-    
+    dev.write(b"SET LED_460_STATUS 1;")
+    dev.write(b"SET LED_670_STATUS 1;")
+
     # Turn on the transmitted LED for imaging
-    dev.write(b'SET LED_TRANS_STATUS 0;')  
-    
+    dev.write(b"SET LED_TRANS_STATUS 0;")
+
     # Wait for 5 seconds to stabilize the lighting conditions
-    tm.sleep(5)  
-    
+    tm.sleep(5)
+
     # Capture a single image with the camera and get timestamps
     images, timestamps = ct.grab_images(cam, n_frames=1)
-    
+
     # Save the captured image as an AVI video using the barcode for identification
-    ct.save_avi(images, barcode=bcode, prefix='algae_growth_curve')
-    
+    ct.save_avi(images, barcode=bcode, prefix="algae_growth_curve")
+
     # Turn the grow lights (460 nm and 670 nm LEDs) back on for algae growth
-    dev.write(b'SET LED_460_STATUS 0;')  
-    dev.write(b'SET LED_670_STATUS 0;')  
-    
+    dev.write(b"SET LED_460_STATUS 0;")
+    dev.write(b"SET LED_670_STATUS 0;")
+
     # Wait for 1 hour (60 minutes * 60 seconds) before the next cycle
-    tm.sleep(60 * 60)  
-    
+    tm.sleep(60 * 60)
+
     # Turn off the transmitted LED after the image capture
-    dev.write(b'SET LED_TRANS_STATUS 1;')  
+    dev.write(b"SET LED_TRANS_STATUS 1;")
 
 # Print the total imaging time (difference between the first and last timestamps)
-print(str(timestamps[-1] - timestamps[0]))  
+print(str(timestamps[-1] - timestamps[0]))
 
 # Print the time between the last two captured frames
 print(str(timestamps[-1] - timestamps[-2]))
 
 # Turn off the transmitted LED at the end of the process
-dev.write(b'SET LED_TRANS_STATUS 1;')
+dev.write(b"SET LED_TRANS_STATUS 1;")
 
 # Release the camera and clean up
 del cam  # Delete the camera object to turn off the camera
